05e68206a7847297125e5e0952895dbbc2cc2f96
Paulo M Serodio <pauloadrianoserodio@gmail.com>
2022-07-26 11:22:44 +0200

    newcourse

--- js/main.js
+++ js/main.js
@@ -40,6 +40,14 @@ $(document).ready(function () {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
+  if (window.location.hash && window.location.hash == '#montevideo2022') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
+
+  if (window.location.pathname && window.location.pathname == '/montevideo2022') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
+
 /*
 if (window.location.pathname && window.location.pathname == '{{ site.url }}') {
     $('.panel-cover').addClass('panel-cover')
